0dfa14cc5db83c774fb580c97755fa3dbf050a9e
Parag Pachpute <paragpachpute3@gmail.com>
2020-06-16 12:47:34 -0400

    Add skeleton for error queue

--- etl/dblp_venue_runner.py
+++ etl/dblp_venue_runner.py
@@ -8,23 +8,46 @@ import urllib.request
 import logging
 import os
 from entity.conference import Conference
+from entity.proceeding import Proceeding
+
+log = logging.getLogger(os.path.basename(__file__))
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
 
 def fetch_proceeding_info(conference_name, venues, parser):
     proceedings = []
     proceeding_xml_base_url = "https://dblp.org/rec/xml/"
     for venue in venues:
         for proceeding_id in venue.proceedings:
-            url = proceeding_xml_base_url + proceeding_id + ".xml"
-            print(url)
-            xml = urllib.request.urlopen(url)
-            proceeding = parser.get_proceeding(conference_name, xml.read())
-            proceedings.append(proceeding)
+            try:
+                url = proceeding_xml_base_url + proceeding_id + ".xml"
+                log.debug("Fetching from proceeding url: %s".format(url))
+                proceeding = get_proceeding_info_from_url(conference_name, url, parser)
+                proceedings.append(proceeding)
+            except Exception as ex:
+                # TODO store the exception in the DB
+                log.error("Parsing error for proceeding_id {} of conference {}. Error: {}".format(proceeding_id, conference_name, ex))
+                pass
     return proceedings
+
+def get_proceeding_info_from_url(conference_name, url, parser):
+    xml = urllib.request.urlopen(url)
+    proceeding = parser.get_proceeding(conference_name, xml.read())
+    return proceeding
+
+
+def get_venues_from_url(url, parser):
+    document = urllib.request.urlopen(url)
+    venues = parser.parse(conference.name, document.read())
+    return venues
+
+
+def get_research_papers_from_url(url):
+    json_doc = urllib.request.urlopen(url)
+    return json.loads(json_doc.read())
 
 
 if __name__ == '__main__':
-    log = logging.getLogger(os.path.basename(__file__))
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
     database = 'test_database'
     parser = VenueParser()
@@ -40,16 +63,22 @@ if __name__ == '__main__':
         log.info('Started processing: ' + conference.name)
 
         if conference.name is not 'akbc':
-            document = urllib.request.urlopen(c['dblp_url'])
-            venues = parser.parse(conference.name, document.read())
-            proceedings = fetch_proceeding_info(conference.name, venues, parser)
-            log.info("Venues and proceedings fetched successfully")
-            venueHome.store_many_venues(venues)
-            proceedingHome.store_many_proceedings(proceedings)
+            try:
+                venues = get_venues_from_url(conference.dblp_url, parser)
+                log.debug("Fetched %d venues for conference %s".format(len(venues), conference.name))
+                venueHome.store_many_venues(venues)
 
+                proceedings = fetch_proceeding_info(conference.name, venues, parser)
+                proceedingHome.store_many_proceedings(proceedings)
+            except Exception as ex:
+                # TODO store the exception in the DB
+                log.error("Parsing error for venues of conference %s".format(conference.name))
+                pass
 
+    # TODO repeat this for each conference
     proceedings = proceedingHome.get_proceedings({"conference_name": "ijcai"})
     for p in proceedings:
+        proceeding = Proceeding(**p)
         log.info("Started processing for " + p['title'])
 
         # TODO generate link to handle more than 1000 records
@@ -60,15 +89,19 @@ if __name__ == '__main__':
         url = base_url + p['dblp_url'].split(".html")[0] + ".bht"
         url += ":&h=1000&format=json"
         print(url)
-        json_doc = urllib.request.urlopen(url)
-        obj = json.loads(json_doc.read())
 
-        if 'result' in obj and 'hits' in obj['result'] and 'hit' in obj['result']['hits']:
-            result = obj['result']
-            hits = result['hits']
-            for research_paper in hits['hit']:
-                # TODO decode html entities from the strings
-                research_paper['_id'] = research_paper['info']['title']
-                researchPaperHome.store_research_paper(research_paper)
+        try:
+            obj = get_research_papers_from_url(url)
 
+            if 'result' in obj and 'hits' in obj['result'] and 'hit' in obj['result']['hits']:
+                result = obj['result']
+                hits = result['hits']
+                for research_paper in hits['hit']:
+                    # TODO decode html entities from the strings
+                    research_paper['_id'] = research_paper['info']['title']
+                    researchPaperHome.store_research_paper(research_paper)
+        except Exception as ex:
+            # TODO store the exception in the DB
+            log.error("Parsing error for research papers of proceeding %s".format(proceeding.proceeding_key))
+            pass
 
