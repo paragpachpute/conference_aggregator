b40acd99b4166e1a2cda7bbe042704bb5b1a05ec
Parag Pachpute <paragpachpute3@gmail.com>
2020-07-08 12:53:10 -0400

    Update test cases

--- parser/dblp/test/test_conference_list_parser.py
+++ parser/dblp/test/test_conference_list_parser.py
@@ -8,6 +8,6 @@ class TestVenueParser(TestCase):
         self.conference_list_parser = ConferenceListParser()
 
     def test_get_venues(self):
-        with open('dblp/conference_list.htm') as document:
+        with open('../resources/conference_list.htm') as document:
             confs = self.conference_list_parser.parse(document.read())
             self.assertEqual(100, len(confs))
--- parser/dblp/test/test_venue_parser.py
+++ parser/dblp/test/test_venue_parser.py
@@ -1,15 +1,21 @@
 from unittest import TestCase
+
 from bs4 import BeautifulSoup
+
 from parser.dblp.venue_parser import VenueParser
+
 
 class TestVenueParser(TestCase):
     def setUp(self) -> None:
         self.venueParser = VenueParser()
 
     def test_get_venues(self):
-        with open('dblp/dblp_aaai.htm') as document:
+        with open('../resources/dblp_aaai.htm') as document:
             soup = BeautifulSoup(document.read(), features="html.parser")
             h2s = soup.find_all('h2')
 
-            venues = self.venueParser.get_venues(h2s)
+            venues = []
+            for h2 in h2s:
+                venue = self.venueParser.get_venue("test", h2)
+                venues.append(venue)
             self.assertEqual(34, len(venues))
