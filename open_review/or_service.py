6bc06b0e539c4fb6228ad3a50665c7e01aba609d
Parag Pachpute <paragpachpute3@gmail.com>
2020-07-18 21:33:04 -0400

    Fetch data from local store instead of OpenReview

--- open_review/or_service.py
+++ open_review/or_service.py
@@ -1,11 +1,22 @@
-import openreview
+from database.transformed_venue_home import TransformedVenueHome
+
 
 class OpenReviewService:
-  def __init__(self):
-    self.client = openreview.Client(baseurl = 'https://dev.openreview.net', username = 'OpenReview.net', password = 'OpenReview_dev')
+    def __init__(self):
+        # self.client = openreview.Client(baseurl='https://dev.openreview.net', username='OpenReview.net',
+        #                                 password='OpenReview_dev')
+        database = 'dev'
+        self.venue_home = TransformedVenueHome(database)
 
-  def store_venue(self, venue):
-    self.client.post_venue(venue)
+    def store_venue(self, venue):
+        self.store_venue(venue)
+        # self.client.post_venue(venue)
 
-  def get_venue(self, ids=None, id=None, invitations=None):
-    return self.client.get_venues(ids=ids, id=id, invitations=invitations)
+    def get_venue(self, ids=None, id=None, invitation=None):
+        criteria = {}
+        if id is not None:
+            criteria['venueid'] = id
+        if invitation is not None:
+            criteria['invitation'] = invitation
+        return self.venue_home.get_venue(criteria)
+        # return self.client.get_venues(ids=ids, id=id, invitations=invitations)
